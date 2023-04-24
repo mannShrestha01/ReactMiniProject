957cb6608b4ae180077b217da616602b14a96c5d
mannShrestha01 <130300983+mannShrestha01@users.noreply.github.com>
2023-04-24 21:33:12 +0545

    Adding fetures in a crt features:Booking,WishList

--- src/App.js
+++ src/App.js
@@ -4,14 +4,54 @@ import Card from "./components/Card";
 import list from "./hotelList.json";
 const App = () => {
   const [hotelsList, setHotelList] = useState(list);
-  const alertMe = (id) => {
-    alert(`hi this ${id} is clicked`);
+  const [isWishListOpen, setIsWishListOpen] = useState(false);
+  const handleFavorite = (id) => {
+    const backUpHotelList = [...hotelsList];
+    backUpHotelList[id].isLiked = !backUpHotelList[id].isLiked;
+    setHotelList(backUpHotelList);
+  };
+
+  const handleBooking = (id) => {
+    const backUpHotelList = [...hotelsList];
+    backUpHotelList[id].bookedCount++;
+    setHotelList(backUpHotelList);
+  };
+
+  const calculateSum = () => {
+    let sum = 0;
+    hotelsList.map((item, id) => {
+      if (item.isLiked) {
+        sum++;
+      }
+    });
+
+    return sum;
+  };
+
+  const handleWishlistOpen = () => {
+    setIsWishListOpen(!isWishListOpen);
   };
 
   return (
     <div>
+      <button onClick={handleWishlistOpen}> WishList</button>
+      {calculateSum()}
+      {isWishListOpen ? (
+        <div className="Wishlist">
+          {hotelsList.map((item, id) => {
+            if (item.isLiked) return <li>{item.name}</li>;
+          })}
+        </div>
+      ) : null}
       {hotelsList.map((item, id) => {
-        return <Card item={item} alertMe={alertMe} id={id} />;
+        return (
+          <Card
+            item={item}
+            handleBooking={handleBooking}
+            handleFavorite={handleFavorite}
+            id={id}
+          />
+        );
       })}
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,23 @@
 import { FaHeart } from "react-icons/fa";
 const Card = (props) => {
-  const clickOnHeart = () => {
-    props.alertMe(props.id);
+  const handleWishList = () => {
+    props.handleFavorite(props.id);
+  };
+  const handleReservation = () => {
+    props.handleBooking(props.id);
   };
   return (
     <div className="Card">
       <h1>{props.id}</h1>
       <img src={props.item.image} className="Hotel" />
       <FaHeart
-        onClick={clickOnHeart}
+        onClick={handleWishList}
         color={props.item.isLiked ? "red" : null}
       />
+      <button onClick={handleReservation}> Book </button>
       <br />
       {props.item.name}
+      {props.item.bookedCount}
       {props.item.address}
       {props.item.description}
     </div>
