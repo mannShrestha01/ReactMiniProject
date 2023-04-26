e866dce8494e9920fd4b24a422462ef4b08fa624
mannShrestha01 <130300983+mannShrestha01@users.noreply.github.com>
2023-04-26 19:24:12 +0545

    Adding trash icon and booking  functions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import "./styles.css";
+import { FaTrash } from "react-icons/fa";
+
 import { useState } from "react";
 import Card from "./components/Card";
 import list from "./hotelList.json";
@@ -6,16 +8,36 @@ const App = () => {
   const [hotelsList, setHotelList] = useState(list);
   const [isWishListOpen, setIsWishListOpen] = useState(false);
   const [isBookListOpen, setIsBookListOpen] = useState(false);
-
+  //1. not doable isBookListOpen = 'ram'
+  //2. always setIsBookListOpen('ram')
   const handleFavorite = (id) => {
     const backUpHotelList = [...hotelsList];
     backUpHotelList[id].isLiked = !backUpHotelList[id].isLiked;
     setHotelList(backUpHotelList);
   };
 
-  const handleBooking = (id) => {
+  const handleBooking = (id, action) => {
     const backUpHotelList = [...hotelsList];
-    backUpHotelList[id].bookedCount++;
+    if (action === "dec") {
+      backUpHotelList[id].bookedCount--;
+    } else {
+      backUpHotelList[id].bookedCount++;
+    }
+    setHotelList(backUpHotelList);
+  };
+
+  const removeParticularBookings = (id) => {
+    const backUpHotelList = [...hotelsList];
+    backUpHotelList[id].bookedCount = 0;
+    setHotelList(backUpHotelList);
+  };
+
+  const removeAllBooking = () => {
+    const backUpHotelList = [...hotelsList];
+    backUpHotelList.map((item, id) => {
+      item.bookedCount = 0;
+      return item;
+    });
     setHotelList(backUpHotelList);
   };
 
@@ -63,22 +85,34 @@ const App = () => {
       ) : null}
 
       {isBookListOpen ? (
-        <div className="BookedList">
+        <table border={1} className="BookedList">
           {hotelsList.map((item, id) => {
             if (item.bookedCount > 0)
               return (
-                <div>
-                  <li>
-                    {item.name}......
-                    {item.price}.....
-                    {item.bookedCount}.....
-                    {item.price * item.bookedCount}
-                  </li>
-                </div>
+                <tr>
+                  <td>{item.name}</td>
+                  <td>{item.price}</td>
+                  <td>
+                    <button onClick={() => handleBooking(id, "inc")}>+</button>
+                  </td>
+                  <td>{item.bookedCount}</td>
+                  <td>
+                    <button onClick={() => handleBooking(id, "dec")}>-</button>
+                  </td>
+                  <td>{item.price * item.bookedCount}</td>
+                  <td>
+                    <FaTrash onClick={() => removeParticularBookings(id)} />
+                  </td>
+                </tr>
               );
           })}
-          total price: {totalPrice()}
-        </div>
+          {totalPrice() > 0 ? (
+            <div>total price: {totalPrice()}</div>
+          ) : (
+            "NO BOOKED LISTS"
+          )}
+          <button onClick={removeAllBooking}>Remove all Bookings</button>
+        </table>
       ) : null}
 
       {hotelsList.map((item, id) => {
